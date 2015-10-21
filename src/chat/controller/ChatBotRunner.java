1c2989fb7d53807b549802fd903f5d6813b97aeb
meanmonkey662 <coolkoch8535@gmail.com>
2015-10-21 12:18:04 -0600

    created chatbot

---
+++ src/chat/controller/ChatBotRunner.java
@@ -0,0 +1,17 @@
+package chat.controller;
+
+/**
+ * Starts the Chatbot program.
+ * @author kkoc6943
+ * @version 1.0 10/21/15
+ * 
+ */
+public class ChatBotRunner
+{
+	public static void main (String[] arg)
+	{
+		ChatBotController myController = new ChatBotController();
+		myController.start();
+	}
+	
+}
