75feebf195826f21c4ef88ac611d6655defb5647
meanmonkey662 <coolkoch8535@gmail.com>
2016-02-23 13:26:04 -0700

    remove side scroll

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -4,20 +4,19 @@ import javax.swing.JPanel;
 
 import java.awt.event.*;
 
-
 import javax.swing.JButton;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.SpringLayout;
 import javax.swing.SwingUtilities;
 import javax.swing.JTextArea;
 import javax.swing.JLabel;
 
-
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 import chat.controller.ChatController;
-
 
 import java.awt.Color;
 
@@ -29,7 +28,9 @@ import java.awt.Color;
 	public class ChatPanel extends JPanel
 	{
 		private ChatController baseController;
+		private JScrollPane textPane;
 		private SpringLayout baseLayout;
+		private SpringLayout baseLayout_1;
 		private JButton submitButton;
 		private JButton submitButton_1;
 		private JTextArea chatArea;
@@ -42,27 +43,42 @@ import java.awt.Color;
  */
 	public ChatPanel(ChatController baseController)
 	{
+		baseLayout = new SpringLayout();
 		this.baseController = baseController;
 		chatArea = new JTextArea(10,30);
-		baseLayout = new SpringLayout();
+		baseLayout_1 = new SpringLayout();
 		submitButton = new JButton("Next");
 		typingField= new JTextField(30);
 		promptLabel = new JLabel("typing to the chatbot oooooff doooommm");
 		submitButton_1 = new JButton("Submit");
 
-	
+		setupChatPane();
 		setupPanel();
 		setupLayout();
 		setupListeners();
 	}
+
+	private void setupChatPane()
+	{
+		chatArea.setLineWrap(true);
+		chatArea.setWrapStyleWord(true);
+		chatArea.setEnabled(false);
+		chatArea.setEditable(false);
+		textPane = new JScrollPane(chatArea);
+		baseLayout_1.putConstraint(SpringLayout.SOUTH, textPane, -20, SpringLayout.SOUTH, this);
+		textPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		textPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+	}
+	
+
 /**
  * adds the components to the JPanel
  */
 	private void setupPanel()
 	{
-		this.setLayout(baseLayout);
+		this.setLayout(baseLayout_1);
 		this.setBackground(Color.blue);
-		this.add(chatArea);
+		this.add(textPane);
 		this.add(typingField);
 		this.add(submitButton_1);
 		this.add(promptLabel);
@@ -74,13 +90,13 @@ import java.awt.Color;
  */
 	private void setupLayout()
 	{
-		baseLayout.putConstraint(SpringLayout.NORTH, submitButton_1, 6, SpringLayout.SOUTH, chatArea);
-		baseLayout.putConstraint(SpringLayout.WEST, submitButton_1, 77, SpringLayout.WEST, this);
-		baseLayout.putConstraint(SpringLayout.EAST, submitButton_1, 333, SpringLayout.WEST, this);
-		baseLayout.putConstraint(SpringLayout.WEST, typingField, 0, SpringLayout.WEST, chatArea);
-		baseLayout.putConstraint(SpringLayout.SOUTH, typingField, -6, SpringLayout.NORTH, chatArea);
-		baseLayout.putConstraint(SpringLayout.NORTH, chatArea, 72, SpringLayout.NORTH, this);
-		baseLayout.putConstraint(SpringLayout.WEST, chatArea, 10, SpringLayout.WEST, this);
+		baseLayout_1.putConstraint(SpringLayout.NORTH, submitButton_1, 6, SpringLayout.SOUTH, chatArea);
+		baseLayout_1.putConstraint(SpringLayout.WEST, submitButton_1, 77, SpringLayout.WEST, this);
+		baseLayout_1.putConstraint(SpringLayout.EAST, submitButton_1, 333, SpringLayout.WEST, this);
+		baseLayout_1.putConstraint(SpringLayout.WEST, typingField, 0, SpringLayout.WEST, chatArea);
+		baseLayout_1.putConstraint(SpringLayout.SOUTH, typingField, -6, SpringLayout.NORTH, chatArea);
+		baseLayout_1.putConstraint(SpringLayout.NORTH, chatArea, 72, SpringLayout.NORTH, this);
+		baseLayout_1.putConstraint(SpringLayout.WEST, chatArea, 10, SpringLayout.WEST, this);
 		
 	}
 /**
