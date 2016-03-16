b00f6b1719a76edfd28e1a464af6b3289584e6f6
meanmonkey662 <coolkoch8535@gmail.com>
2016-03-16 11:33:44 -0600

    added to controller

--- src/chat/controller/ChatController.java
+++ src/chat/controller/ChatController.java
@@ -1,5 +1,6 @@
 package chat.controller;
 
+import twitter4j.TwitterException;
 import chat.model.CTECTwitter;
 import chat.model.Chatbot;
 import chat.view.*;
@@ -111,6 +112,15 @@ public class ChatController
 	public String analyze(String userName)
 	{
 		String userAnalysis = "The Twitter user " + userName + "has...";
+		try
+		{
+			myTwitter.loadTweets(userName);
+		}
+		catch (TwitterException error)
+		{
+			handleErrors(error.getErrorMessage());
+		}
+		userAnalysis += myTwitter.topResults();
 		
 		return userAnalysis;
 	}
--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -1,92 +1,144 @@
 package chat.model;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 import twitter4j.*;
 import chat.controller.ChatController;
-public class CTECTwitter
-{
+
+import java.util.*;
+
+public class CTECTwitter {
 	private ArrayList<Status> statusList;
 	private ArrayList<String> wordsList;
 	private Twitter chatBotTwitter;
 	private ChatController baseController;
-	
-	
-public CTECTwitter(ChatController baseController)
-{
-	this.baseController = baseController;
-	statusList = new ArrayList<Status>();
-	wordsList = new ArrayList<String>();
-	this.chatBotTwitter = TwitterFactory.getSingleton();
-}
 
-public void sendTweet(String message)
-{	
-	try
-	{
-		chatBotTwitter.updateStatus("Dylan Rockne #APCSRocks @CTECNow Thanks @cscheerleader & @codyhenrichsen!");
+	public CTECTwitter(ChatController baseController) {
+		this.baseController = baseController;
+		statusList = new ArrayList<Status>();
+		wordsList = new ArrayList<String>();
+		this.chatBotTwitter = TwitterFactory.getSingleton();
 	}
-	catch(TwitterException error)
-	{
-		baseController.handleErrors(error.getErrorMessage());
-	}
-	
-}
 
-public void loadTweets(String twitterHandle) throws TwitterException
-{
-	Paging statusPage = new Paging(1, 200);
-	int page = 1;
-	while(page <= 10)
-	{
-		statusPage.setPage(page);
-		statusList.addAll(chatBotTwitter.getUserTimeline(twitterHandle, statusPage));
-		page++;
-	}
-	for(Status currentStatus : statusList)
-	{
-		String[] tweetText = currentStatus.getText().split("");
-		for(String word : tweetText)
-		{
-			wordsList.add(removePunctuation(word).toLowerCase());
+	public void sendTweet(String message) {
+		try {
+			chatBotTwitter
+					.updateStatus("Kyle Koch #APCSRocks @CTECNow Thanks @cscheerleader & @codyhenrichsen!");
+		} catch (TwitterException error) {
+			baseController.handleErrors(error.getErrorMessage());
 		}
-				
+
 	}
-	removeCommonEnglishWords(wordsList);
-	removeEmptyText();
-}
-	private void removeCommonEnglishWords(List<String>wordsList)
-	{
-		
+
+	public void loadTweets(String twitterHandle) throws TwitterException {
+		Paging statusPage = new Paging(1, 200);
+		int page = 1;
+		while (page <= 10) {
+			statusPage.setPage(page);
+			statusList.addAll(chatBotTwitter.getUserTimeline(twitterHandle,
+					statusPage));
+			page++;
+		}
+		for (Status currentStatus : statusList) {
+			String[] tweetText = currentStatus.getText().split("");
+			for (String word : tweetText) {
+				wordsList.add(removePunctuation(word).toLowerCase());
+			}
+
+		}
+		removeCommonEnglishWords(wordsList);
+		removeEmptyText();
 	}
-	
-	private void removeEmptyText()
-	{
-		for (int spot = 0; spot < wordsList.size(); spot++)
-		{
-			if(wordsList.get(spot).equals(""))
+
+	private void removeCommonEnglishWords(List<String> wordsList) {
+		String[] boringWords = importWordsToArray();
+
+		for (int count = 0; count < wordsList.size(); count++) {
 			{
+				for (int removeSpot = 0; removeSpot < boringWords.length; removeSpot++) {
+					if (wordsList.get(count).equalsIgnoreCase(
+							boringWords[removeSpot])) {
+						wordsList.remove(count);
+						count--;
+						removeSpot = boringWords.length;
+					}
+				}
+			}
+		}
+	}
+
+	private void removeEmptyText() {
+		for (int spot = 0; spot < wordsList.size(); spot++) {
+			if (wordsList.get(spot).equals("")) {
 				wordsList.remove(spot);
 				spot--;
 			}
 		}
 	}
-	
-	private String removePunctuation(String currentString)
-	{
+
+	private String removePunctuation(String currentString) {
 		String punctuation = ".,'/!:;\"(){}^<>-";
-		
+
 		String scrubbedString = "";
-		for (int i = 0; i < currentString.length(); i++)
-		{
-			if(punctuation.indexOf(currentString.charAt(i)) == -1)
-			{
-				 scrubbedString += currentString.charAt(i);
+		for (int i = 0; i < currentString.length(); i++) {
+			if (punctuation.indexOf(currentString.charAt(i)) == -1) {
+				scrubbedString += currentString.charAt(i);
 			}
 		}
-		return  scrubbedString;
+		return scrubbedString;
+	}
 
-		
+	private String[] importWordsToArray() {
+		String[] boringWords;
+		int wordCount = 0;
+		try {
+			Scanner wordFile = new Scanner(new File("commonWords.txt"));
+			while (wordFile.hasNext()) {
+				wordCount++;
+				wordFile.next();
+			}
+			wordFile.reset();
+			boringWords = new String[wordCount];
+			int boringWordCount = 0;
+			while (wordFile.hasNext()) {
+				boringWords[boringWordCount] = wordFile.next();
+				boringWordCount++;
+			}
+			wordFile.close();
+		} catch (FileNotFoundException e) {
+			baseController.handleErrors(e.getMessage());
+			return new String[0];
+		}
+		return boringWords;
+	}
+
+	public String topResults()
+	{
+		String tweetResults = "";
+		int topWordLocation = 0;
+		int topCount = 0;
+		for (int index = 0; index < wordsList.size(); index++)
+		{
+			int wordUseCount = 1;
+			for (int spot = index + 1; spot < wordsList.size(); index++)
+			{
+				if (wordsList.get(index).equals(wordsList.get(spot)))
+				{
+					wordUseCount++;
+				}
+				if (wordUseCount > topCount)
+				{
+					topCount = wordUseCount;
+					topWordLocation = index;
+				}
+			}
+		}
+		tweetResults = "The top word in the tweets was "
+				+ wordsList.get(topWordLocation) + " and it was used "
+				+ topCount + " times!";
+		return tweetResults;
+
 	}
 }
-
